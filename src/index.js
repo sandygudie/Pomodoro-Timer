070dbd30e2407e1a0073d9eafd3e12c262da120f
sandygudie <goodnewssandy@gmail.com>
2022-09-03 14:25:48 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+
 import '/src/sass/style.scss'
+
+// $fa-font-path: '~@fortawesome/fontawesome-free/webfonts'; 
+// import '~@fortawesome/fontawesome-free/scss/fontawesome';
+// import '~@fortawesome/fontawesome-free/scss/regular';
+// import '~@fortawesome/fontawesome-free/scss/solid';
 import { settings } from './settings.js'
 import { timer } from './timer.js'
 
--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,14 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+        use: [
+          {
+            loader: 'file-loader',
+          },
+    ],
+  
+  },
+      
     ],
   },
 }
